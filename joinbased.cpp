74f5892bd193ec2d35a284c95b1f417566f377b9
xuliang-chinatelecom <xuliang-chinatelecom@local.com>
2022-11-15 16:50:42 +0800

    gen table ins

--- joinbased.cpp
+++ joinbased.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <cmath>
 #include <map>
+#include <set>
 using namespace std;
 
 //保存位置信息
@@ -16,7 +17,8 @@ struct SpaceInstance{
     int InstanceID;//{1,2,}
     Position Location;//{x, y}
     string FeatureType;//{A}
-    SpaceInstance(int id, Position p, string FeatureType) : InstanceID(id), Location(p), FeatureType(FeatureType) {};
+    SpaceInstance(int id, Position p, string FeatureType) : 
+        InstanceID(id), Location(p), FeatureType(FeatureType) {};
 };
 
 class JoinBased{
@@ -25,18 +27,19 @@ private:
     vector<string> ET;
     double min_prev;
     double min_conf;
-    double R;
-    int k;//co-location的阶
-    vector<SpaceInstance> Ck;
-    map<string, int> count_et;
-
+    double R;//距离R
+    map<string, int> count_et;//保存空间特征出现次数
+    // int k;//co-location的阶
+    // vector<SpaceInstance> Ck;//k阶候选co-location集
+    // map<set<int>, set<int> > Tk;//Ck中co-location的表实例集
+    // vector<vector<string> > Pk;//k阶频繁co-location集
 
 public:
-    JoinBased(const vector<SpaceInstance> E, vector<string> ET, double min_prev, double min_conf, double R) : 
+    JoinBased(const vector<SpaceInstance> &E, const vector<string> &ET, double min_prev, double min_conf, double R) : 
         E(E), ET(ET), min_prev(min_prev), min_conf(min_conf), R(R) {};
-    void getCountET();
+    void getCountET();//计算每个元素出现的次数，用于求参考率
     bool isNiber(Position a, Position b);//计算两个点的距离，如果满足R返回true，否则返回false;
-
+    vector<set<int> > gen_table_ins(const vector<string> &C1, const vector<SpaceInstance> &E);
 };
 
 void JoinBased::getCountET(){
@@ -60,14 +63,28 @@ bool JoinBased::isNiber(Position a, Position b){
     }
     return false;
 }
-
+//表里存放数字就行了
+vector<set<int> > JoinBased::gen_table_ins(const vector<string> &C1, const vector<SpaceInstance> &E){
+    
+}
 int main(){
     vector<SpaceInstance> E{
         SpaceInstance(1, Position(24,14), "A"),
         SpaceInstance(2, Position(64,44), "A"),
         SpaceInstance(3, Position(46,24), "A"),
-        SpaceInstance(4, Position(63,7), "A")
+        SpaceInstance(4, Position(63,7), "A"),
+        SpaceInstance(1, Position(13,3), "B"),
+        SpaceInstance(2, Position(19,45), "B"),
+        SpaceInstance(3, Position(47.5,10.5), "B"),
+        SpaceInstance(4, Position(62,52), "B"),
+        SpaceInstance(5, Position(13,50), "B"),
+        SpaceInstance(1, Position(39,11), "C"),
+        SpaceInstance(2, Position(52,50), "C"),
+        SpaceInstance(3, Position(40,36), "C"),
+        SpaceInstance(1, Position(40,20), "D"),
+        SpaceInstance(2, Position(6,10), "D")
     };
-    vector<string> ET{"A"};
-    // JoinBased j(E, ET, 0.3, 0.3);
+    vector<string> ET{"A", "B", "C", "D"};
+    JoinBased j(E, ET, 0.3, 0.3, 12);
+    j.getCountET();
 }
